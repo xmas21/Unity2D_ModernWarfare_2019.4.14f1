b4383098c726fd8d9a3288724196983ba9df10a0
HWChia <66512878+xmas21@users.noreply.github.com>
2020-11-21 21:10:45 +0800

    程式基礎 | 腳色移動

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -2,14 +2,14 @@
 
 public class Player : MonoBehaviour
 {
-    #region
+    #region 欄位
     [Header("移動速度"), Range(0, 1000)]
     public float speed = 10.5f;
     [Header("跳要高度"), Range(0, 3000)]
     public float jump = 100f;
     [Header("是否在地板上")]
     public bool IsGround = false;
-    [Header("子彈"),Tooltip("射出的子彈")]
+    [Header("子彈"), Tooltip("射出的子彈")]
     public GameObject bullet;
     [Header("子彈生成點"), Tooltip("生成子彈的位置")]
     public Transform point;
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     public float BulletSpeed = 800;
     [Header("開槍音效"), Tooltip("射擊的音效")]
     public AudioClip SoundFIre;
-    [Header("生命數量"), Range(0,10)]
+    [Header("生命數量"), Range(0, 10)]
     public int HpNumber = 3;
 
     private int score = 0;
@@ -26,6 +26,11 @@ public class Player : MonoBehaviour
     private Animator ani;
     #endregion
 
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
     private void Start()
     {
         Drive();
@@ -33,7 +38,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+
     }
 
     private void Drive()
@@ -41,5 +46,37 @@ public class Player : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        float v = Input.GetAxis("Horizontal");
+        rig.velocity;
+    }
 
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    /// 開槍
+    /// </summary>
+    private void Fire()
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡 
+    /// </summary>
+    /// <param name="obj">碰到的物件</param>
+    private void Dead(string obj)
+    {
+
+    }
 }
